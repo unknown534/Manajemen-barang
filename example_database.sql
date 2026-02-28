d946e1719d6ad11e4bd39efe0e1d1d9610bde8ae
jakajekbae <56154522+unknown534@users.noreply.github.com>
2026-03-01 05:45:20 +0700

    Update password for user 'jakajekbae'

--- example_database.sql
+++ example_database.sql
@@ -1893,7 +1893,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
-(1, 'jakajekbae', 'jakaakaj', 'admin'),
+(1, 'jakajekbae', 'jaka123', 'admin'),
 (2, 'gugun', 'gugun123', 'admin');
 
 --
